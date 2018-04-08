ce0250fecfca4758e8bd45c6dd4b6f24cef01761
thejoshest <jhoop.acb@gmail.com>
2018-04-07 22:34:38 -0400

    added custom iterator to custom object

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,36 @@
-function baz() {
-    console.log( "baz" );
-    bar();
-}
-
-function bar() {
-    console.log( "bar" );
-    foo();
-}
 
 function foo() {
-    console.log( "foo" );
     var el = document.getElementById('main');
     el.innerHTML = "Can't get enough";
 }
 
-baz();
+foo();
+
+var favorites = {
+    Guitar : "Gretsch",
+    Person : "Brooke",
+    Game : "Overwatch",
+    Food : "Burgers"   
+}
+
+Object.defineProperty(favorites, Symbol.iterator, {
+    enumerable:false,
+    writable:false,
+    configurable:true,
+    value: funtion(){
+        var o = this,
+          idx = 0,
+           ks = Object.keys(0);
+        return {
+            next: function() {
+                return {    
+                    value:o[ks[idx++]],
+                    done: (idx > ks.length)
+                };
+            }
+        };
+    }
+});
+for (var v of favorites) {
+    console.log(v);
+}
